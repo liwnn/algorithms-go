994e60ecce50febd8063f19fc5046578686572b0
李伟宁(蓝蜗牛) <liweining@yoozoo.com>
2021-07-21 13:20:34 +0800

    skiplist

--- skiplist/skiplist.go
+++ skiplist/skiplist.go
@@ -4,47 +4,46 @@ import (
 	"math/rand"
 )
 
-// node is an element of a skip list
-type node struct {
+const (
+	kMaxLevel = 12   // (1/p)^MaxLevel >= maxNode
+	kP        = 0.25 // Skiplist P = 1/4
+)
+
+// skipListNode is an element of a skip list
+type skipListNode struct {
 	key     int
 	value   int
-	forward []*node
+	forward []*skipListNode
 }
 
-func makeNode(level int, key int, value int) *node {
-	return &node{
+func newNode(level int, key int, value int) *skipListNode {
+	return &skipListNode{
 		key:     key,
 		value:   value,
-		forward: make([]*node, level),
+		forward: make([]*skipListNode, level),
 	}
 }
 
-// SkipList represents a skip list
+// SkipList implemente "Skip Lists: A Probabilistic Alternative to Balanced Trees"
 type SkipList struct {
-	header   *node
-	level    int // current level count
-	maxLevel int
-	p        float64
-
-	update []*node // for less alloc
+	header *skipListNode
+	level  int // current max level
 }
 
 // NewSkipList creates a skip list
 func NewSkipList() *SkipList {
 	sl := &SkipList{
-		level:    1,
-		maxLevel: 8,    // (1/p)^maxLevel >= maxNode
-		p:        0.25, // Skiplist P = 1/4
+		level:  1,
+		header: newNode(kMaxLevel, 0, 0),
 	}
-	sl.header = makeNode(sl.maxLevel, 0, 0)
-	sl.update = make([]*node, sl.maxLevel)
 	return sl
 }
 
 // Search for an element by traversing forward pointers
-func (list *SkipList) Search(searchKey int) (int, bool) {
-	x := list.header
-	for i := list.level - 1; i >= 0; i-- {
+func (sl *SkipList) Search(searchKey int) (int, bool) {
+	x := sl.header
+	// loop : x→key < searchKey <= x→forward[i]→key
+	for i := sl.level - 1; i >= 0; i-- {
 		for x.forward[i] != nil && x.forward[i].key < searchKey {
 			x = x.forward[i]
 		}
@@ -57,62 +56,62 @@ func (list *SkipList) Search(searchKey int) (int, bool) {
 }
 
 // Insert element
-func (list *SkipList) Insert(searchKey int, newValue int) {
-	var update = list.update // [0...list.maxLevel)
-	x := list.header
-	for i := list.level - 1; i >= 0; i-- {
+func (sl *SkipList) Insert(searchKey int, newValue int) {
+	var prev [kMaxLevel]*skipListNode // [0...list.maxLevel)
+	x := sl.header
+	for i := sl.level - 1; i >= 0; i-- {
 		for x.forward[i] != nil && x.forward[i].key < searchKey {
 			x = x.forward[i]
 		}
-		update[i] = x
+		prev[i] = x
 	}
 	x = x.forward[0]
 	if x != nil && x.key == searchKey {
 		x.value = newValue
 	} else {
-		lvl := list.randomLevel()
-		if lvl > list.level {
-			for i := list.level; i < lvl; i++ {
-				update[i] = list.header
+		lvl := sl.randomLevel()
+		if lvl > sl.level {
+			for i := sl.level; i < lvl; i++ {
+				prev[i] = sl.header
 			}
-			list.level = lvl
+			sl.level = lvl
 		}
 
-		x = makeNode(lvl, searchKey, newValue)
+		x = newNode(lvl, searchKey, newValue)
 		for i := 0; i < lvl; i++ {
-			x.forward[i] = update[i].forward[i]
-			update[i].forward[i] = x
+			x.forward[i] = prev[i].forward[i]
+			prev[i].forward[i] = x
 		}
 	}
 }
 
 // Delete element
-func (list *SkipList) Delete(searchKey int) {
-	var update = list.update // [0...list.maxLevel)
-	x := list.header
-	for i := list.level - 1; i >= 0; i-- {
+func (sl *SkipList) Delete(searchKey int) {
+	var prev [kMaxLevel]*skipListNode // [0...list.maxLevel)
+	x := sl.header
+	for i := sl.level - 1; i >= 0; i-- {
 		for x.forward[i] != nil && x.forward[i].key < searchKey {
 			x = x.forward[i]
 		}
-		update[i] = x
+		prev[i] = x
 	}
 	x = x.forward[0]
 	if x != nil && x.key == searchKey {
-		for i := 0; i < list.level; i++ {
-			if update[i].forward[i] != x {
+		for i := 0; i < sl.level; i++ {
+			if prev[i].forward[i] != x {
 				break
 			}
-			update[i].forward[i] = x.forward[i]
+			prev[i].forward[i] = x.forward[i]
 		}
-		for list.level > 1 && list.header.forward[list.level-1] == nil {
-			list.level--
+		for sl.level > 1 && sl.header.forward[sl.level-1] == nil {
+			sl.level--
 		}
 	}
 }
 
-func (list *SkipList) randomLevel() int {
+func (sl *SkipList) randomLevel() int {
 	lvl := 1
-	for rand.Float64() < list.p && lvl < list.maxLevel {
+	for lvl < kMaxLevel && rand.Float64() < kP {
 		lvl++
 	}
 	return lvl
